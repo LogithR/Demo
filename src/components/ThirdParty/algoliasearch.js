6f4f00c627eed52bb00a44e852625c29be1be542
Logith <logithramesh4444@gmail.com>
2024-06-28 17:43:39 +0530

    Second Commit

--- src/components/ThirdParty/algoliasearch.js
+++ src/components/ThirdParty/algoliasearch.js
@@ -8,7 +8,7 @@ import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-d
 const searchClient = algoliasearch('A50UWSCP7X', '6ae9e3b58c9eea39f6aed70049943a3b');
 
 const Hit = ({ hit }) => (
-  <div style={{textAlign:'center'}}>
+  <div>
     <h2>
       <Highlight attribute="firstname" hit={hit} /> <Highlight attribute="lastname" hit={hit} />
     </h2>
